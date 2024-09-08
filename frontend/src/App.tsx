005bfac6e0a953f2263645578d614c8dc6fce6d1
Zach <zach@jzimm.com>
2024-09-07 21:10:07 -0700

    Added theme support and added a build script without tsc

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,7 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { text } from 'stream/consumers'
+import ModeToggle from './components/ui/mode-toggle'
 
 function App() {
   const [chatContent, setChatContent] = useState("")
@@ -45,13 +46,15 @@ function App() {
 
   return (
     <div className='h-[90vh] w-[90vw] items-center justify-center'>
+
       <ResizablePanelGroup direction="horizontal"
       className="justify-self-center h-full rounded-lg border">
         <ResizablePanel className='w-full'>
+          {/*Chat Panel
+              This should probably be in a seperate component / file*/}
           <div className="flex flex-col h-full justify-items-center p-6">
-          <h1 className='font-semibold justify-self-center pb-4'>Kocoon</h1>
-          
-          <Card className="flex-1 flex flex-col pt-4">
+            <h1 className='font-semibold justify-self-center pb-4'>Kocoon</h1>         
+            <Card className="flex-1 flex flex-col pt-4">
             <div className='px-4'>
               <span className="text-lg font-semibold">Chat:</span>
             </div>
@@ -75,11 +78,19 @@ function App() {
       </ResizablePanel>
         <ResizableHandle withHandle/>
         <ResizablePanel defaultSize={50} className='w-full'>
+          { /* Secondary panel
+                This will probably contain charts or something,
+                but it will likely end up in a seperate component too */ }
           <div className='flex-auto w-full h-full items-center justify-center p-6'>
             <h1 className='font-semibold justify-self-center pb-4'>Some Buttons</h1>
+            {/* These buttons just show how convinient shadcn is,
+                  especially how they change with the selected theme*/}
             <Button variant="default">Primary</Button>
             <Button variant="secondary">Secondary</Button>
             <Button variant="outline">Outline</Button>
+            <Button variant="destructive">Warning</Button>
+            <br />
+            <ModeToggle />
           </div>
           
         </ResizablePanel>
